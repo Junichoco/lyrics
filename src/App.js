f1f9e2e09e7b6b36aaecb02860b9770cde36516b
unknown <sunjun.software@gmail.com>
2026-07-26 11:39:37 +0900

    Koi wa Illusion

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
             <a class="button" onClick={() => changeSong("Rain")}>Rain</a>
             <a class="button" onClick={() => changeSong("SPECIALZ")}>SPECIALZ</a>
             <a class="button" onClick={() => changeSong("Hai Yorokonde")}>Hai Yorokonde</a>
-            <a class="button" onClick={() => changeSong("monkey")}>Koi wa Illusion</a>
+            <a class="button" onClick={() => changeSong("Koi wa Illusion")}>Koi wa Illusion</a>
             <a class="button" onClick={() => changeSong("monkey")}>monkey</a>
           </div>
           <div class="lyrics">
